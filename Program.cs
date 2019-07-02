498e267f699613c41261fb9210bb3232e5514a1a
gugu <gustavocorrea2032@gmail.com>
2019-07-02 11:49:12 -0300

    Localizando um valor em um vetor

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Vetor5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int [] numeros = {0, 2, 4, 6, 8, 10};
+            int requerido = 4;
+            int n;
+            for ( n = 0 ; n <=5 ; n++) {
+                if (numeros[n] == requerido){
+                Console.WriteLine (n);
+                }
+            }
+        }
+    }
+}
